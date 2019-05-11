210faeee206848c2c141e01b1e216d4340b3d416
MarcinBalejko <mbbm9@o2.pl>
2019-05-12 00:22:57 +0800

    :white_check_mark: Lands on indicated airport

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,9 +8,15 @@ describe Plane do
     it 'responds to land method' do
         expect(subject).to respond_to :land
     end
+
     it 'ready to land' do
         subject.land(airport)
         expect(subject.landed?).to be true
     end
-    
+
+    it 'lands on the indicated airport' do
+        subject.land(airport)
+        expect(airport.store_plane(subject)[0]).to be subject
+    end
+
 end
