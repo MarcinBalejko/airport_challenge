f2ebad8192353599ccf03dd77146bf520a23d2b2
MarcinBalejko <mbbm9@o2.pl>
2019-05-12 02:47:37 +0800

    :recycle: Refactored

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,5 @@
 require_relative 'airport'
 class Plane
-    def initialize
-    @landed = false
-    end
     def land(airport)
         @landed = true
         fail 'Airport is full' if airport.full? != false
