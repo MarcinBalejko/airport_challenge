7cef80339922889ae85b53f14cd9eef9ab2e9eff
MarcinBalejko <mbbm9@o2.pl>
2019-05-11 22:08:26 +0800

    :white_check_mark: Plane is able to land

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,6 +1,9 @@
 
 class Plane
     def land
-
+        @landed = true
+    end
+    def landed?
+        @landed
     end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,5 +4,8 @@ describe Plane do
     it 'responds to land method' do
         expect(subject).to respond_to :land
     end
-    
+    it 'lands' do
+        subject.land
+        expect(subject.landed?).to be true
+    end
 end
