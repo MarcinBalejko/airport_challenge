f66e0eea1cc6584ee5a0fd8bb0ddb12a27cf2a2e
MarcinBalejko <mbbm9@o2.pl>
2019-05-13 22:54:06 +0800

    :fire: Plane class modified

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,4 @@
 require_relative 'airport'
 class Plane
-    def land(airport)
-        @landed = true
-        fail 'Airport is full' if airport.full? != false
-        
-    end
-    def landed?
-        @landed
-    end
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,28 +1,4 @@
 require './lib/plane.rb'
-require './lib/airport.rb'
-
 describe Plane do
-
-    let(:airport) {Airport.new}
-
-    it 'responds to land method' do
-        expect(subject).to respond_to :land
-    end
-
-    it 'ready to land' do
-        subject.land(airport)
-        expect(subject.landed?).to be true
-    end
-
-    it 'lands on the indicated airport' do
-        subject.land(airport)
-        expect(airport.store_plane(subject)[0]).to be subject
-    end
-
-    it 'raises an error if airport is full' do
-        subject.land(airport)
-        airport.capacity.times { airport.store_plane(subject)} 
-        expect{subject.land(airport)}.to raise_error 'Airport is full'
-    end
 
 end
