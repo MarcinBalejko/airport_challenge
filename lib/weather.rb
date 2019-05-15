4225d4244c4e691220f7966cd2e586edf3fe3e07
MarcinBalejko <mbbm9@o2.pl>
2019-05-16 00:23:37 +0800

    :white_check_mark: Weather can be forecasted

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,5 +1,6 @@
 require_relative 'airport'
 class Weather
+    attr_reader :state
     def initialize
         @type = ['stormy', 'sunny']
         @state = @type.sample
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -2,6 +2,9 @@ require './lib/weather.rb'
 describe Weather do
     it 'it can be stormy or sunny' do
         weather = Weather.new
-        expect(weather.forecast).to eq("sunny").or eq("stormy")
+        expect(weather.state).to eq("sunny").or eq("stormy")
+    end
+    it 'can be forecasted' do
+        expect(subject.forecast).to eq("sunny").or eq("stormy")
     end
 end
