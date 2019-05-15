ff96fc38e7fc9ac2c166f5ad57db1c9b28a28dc7
MarcinBalejko <mbbm9@o2.pl>
2019-05-16 00:36:41 +0800

    :white_check_mark: forecast from the airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,12 @@
 require_relative 'plane'
+require_relative 'weather'
 class Airport
-    attr_reader :capacity, :planes
+    attr_reader :capacity, :planes, :weather
     DEFAULT_CAPACITY = 3
     def initialize(capacity = DEFAULT_CAPACITY)
         @capacity = capacity
         @planes = []
+        @weather = Weather.new
     end
     def store_plane(plane)
         fail 'Airport is full' if full?
@@ -14,7 +16,9 @@ class Airport
         @planes.delete(plane)
         "Plane #{plane} departed"
     end
-
+    def weather_forecast
+        @weather
+    end
     def full?
         @planes.count >= @capacity
     end
