ab63076bb2af79eda68adc8da1f59d58756eb5ec
MarcinBalejko <mbbm9@o2.pl>
2019-05-18 02:14:48 +0800

    :white_check_mark: plane is able to land

--- lib/plane.rb
+++ lib/plane.rb
@@ -6,5 +6,7 @@ class Plane
     def take_off
         raise 'Cannot take off: Plane is already flying'
     end
+    def land(airport)
+    end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,11 +1,18 @@
 require './lib/plane.rb'
 describe Plane do
 
+    let(:airport) {Airport.new}
+
     describe '#takeoff' do
         it { is_expected.to respond_to :take_off }
 
         it 'it raises an error when flying' do
             expect{ subject.take_off }.to raise_error 'Cannot take off: Plane is already flying'
+        end
+    end
+    describe '#land' do
+        it 'responds to land' do
+            expect(subject).to respond_to(:land).with(1).argument
         end
     end
 end
