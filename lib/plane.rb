a37993e4a7b2603e5126c77cc4e552f86c105f39
MarcinBalejko <mbbm9@o2.pl>
2019-05-17 23:35:57 +0800

    :white_check_mark: raises error when flying

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,7 @@
 require_relative 'airport'
 class Plane
+    def take_off
+        raise 'Cannot take off: Plane is already flying'
+    end
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,4 +1,9 @@
 require './lib/plane.rb'
 describe Plane do
 
+    describe '#takeoff' do
+        it 'it raises an error when flying' do
+            expect{ subject.take_off }.to raise_error 'Cannot take off: Plane is already flying'
+        end
+    end
 end
